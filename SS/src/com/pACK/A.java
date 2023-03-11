668c0f63a6b924c0fc320f5b67eec969e59dbbcf
Sarang <bhongsarang007@gmail.com>
2023-03-11 09:21:32 +0530

    array code

---
+++ SS/src/com/pACK/A.java
@@ -0,0 +1,16 @@
+package com.pACK;
+
+import java.util.ArrayList;
+
+public class A {
+
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> a=new ArrayList<>();
+		a.add(22);
+		a.add(12);
+		
+		System.out.println(a);
+	}
+}
